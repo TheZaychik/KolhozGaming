2656bb860b397bfde3a68810fef34b9fd41d3371
acematty <vesnin95@gmail.com>
2019-12-06 12:28:25 +0300

    Ебанный рот

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from main import views
 from django.conf.urls import url
 
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
